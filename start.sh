a0baf5e1cb01af75ed15c37aff917c6c2f4d6aa7
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-05-15 00:31:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-python3 -m bot
+echo "Cloning Repo...."
+git clone https://github.com/konichiwa55115/vidcomp626622 /LazyDeveloper
+cd /LazyDeveloper
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+gunicorn app:app & python3 bot.py
